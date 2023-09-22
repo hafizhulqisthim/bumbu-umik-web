b891c2c08abeb9fa2f124eed2a48b84465efc2cd
andikatuluspangestu <andikatulusp@gmail.com>
2023-09-22 20:11:12 +0700

    Changes : Implementasi Modularisasi Berkas

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#header").load("partials/header.html");
+  $("#hero").load("partials/hero.html");
+  $("#about").load("partials/about.html");
+  $("#keunggulan").load("partials/keunggulan.html");
+  $("#products").load("partials/products.html");
+  $("#testimonial").load("partials/testimonial.html");
+  $("#call-to-action").load("partials/cta.html");
+  $("#kritiksaran").load("partials/kritiksaran.html");
+  $("#footer").load("partials/footer.html");
+});
