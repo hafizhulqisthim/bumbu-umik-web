062a67006d8ba57cfe3205188011b88b07d0ff82
ekanugraha0211 <ekanugraha0211@gmail.com>
2023-10-03 20:08:33 +0700

    link shopee

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -41,3 +41,14 @@ $(document).ready(function () {
 
   typeEffect();
 });
+
+document.getElementById('belanja-sekarang').addEventListener('click', function (event) {
+  event.preventDefault(); 
+
+  var konfirmasi = confirm('Anda akan diarahkan ke halaman baru. Lanjutkan?');
+
+  if (konfirmasi) {
+    window.open('https://shopee.co.id/Bumbu-Umik-Bumbu-Instan-Bumbu-Dapur-Penyedap-Rasa-Makanan-65Gram-i.926276408.23419570179?sp_atk=2ac064fd-8f1b-4ef3-9e1c-7367c4fcf0a0&xptdk=2ac064fd-8f1b-4ef3-9e1c-7367c4fcf0a0','_BLANK')
+  }
+});
+      
